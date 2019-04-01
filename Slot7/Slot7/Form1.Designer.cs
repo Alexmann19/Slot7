6520f873611f756678c77189255ed1f3e5dacffd
Alexander Lehr <Alexlehr@hotmail.de>
2019-04-01 10:58:37 +0200

    add programm

---
+++ Slot7/Slot7/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Slot7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbslot1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbslot2 = new System.Windows.Forms.TextBox();
+            this.tbslot3 = new System.Windows.Forms.TextBox();
+            this.tbpunkte = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbslot1
+            // 
+            this.tbslot1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbslot1.Location = new System.Drawing.Point(60, 84);
+            this.tbslot1.MaxLength = 1;
+            this.tbslot1.Name = "tbslot1";
+            this.tbslot1.ReadOnly = true;
+            this.tbslot1.ShortcutsEnabled = false;
+            this.tbslot1.Size = new System.Drawing.Size(30, 38);
+            this.tbslot1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbslot3);
+            this.groupBox1.Controls.Add(this.tbslot2);
+            this.groupBox1.Controls.Add(this.tbslot1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(370, 208);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Slots";
+            // 
+            // tbslot2
+            // 
+            this.tbslot2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbslot2.Location = new System.Drawing.Point(168, 84);
+            this.tbslot2.MaxLength = 1;
+            this.tbslot2.Name = "tbslot2";
+            this.tbslot2.ReadOnly = true;
+            this.tbslot2.ShortcutsEnabled = false;
+            this.tbslot2.Size = new System.Drawing.Size(30, 38);
+            this.tbslot2.TabIndex = 1;
+            // 
+            // tbslot3
+            // 
+            this.tbslot3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbslot3.Location = new System.Drawing.Point(284, 84);
+            this.tbslot3.MaxLength = 1;
+            this.tbslot3.Name = "tbslot3";
+            this.tbslot3.ReadOnly = true;
+            this.tbslot3.ShortcutsEnabled = false;
+            this.tbslot3.Size = new System.Drawing.Size(30, 38);
+            this.tbslot3.TabIndex = 2;
+            // 
+            // tbpunkte
+            // 
+            this.tbpunkte.Location = new System.Drawing.Point(254, 227);
+            this.tbpunkte.Name = "tbpunkte";
+            this.tbpunkte.ReadOnly = true;
+            this.tbpunkte.Size = new System.Drawing.Size(129, 20);
+            this.tbpunkte.TabIndex = 2;
+            this.tbpunkte.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(204, 230);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Punkte:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(254, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(13, 227);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(185, 98);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Punkte Legende";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "7 = 60 Punkte";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "3x";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "S = 30 Punkte";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "+ = 20 Punkte";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 68);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "! = 10 Punkte";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 81);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "# = 5 Punkte";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 332);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbpunkte);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Slot7";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbslot3;
+        private System.Windows.Forms.TextBox tbslot2;
+        private System.Windows.Forms.TextBox tbpunkte;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbslot1;
+    }
+}
+
---
+++ Slot7/Slot7/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Slot7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random zufall = new Random();
+
+        public static int punkte = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int zufallzahl = Convert.ToInt32(zufall.Next(1, 11));
+
+            if (zufallzahl == 1)
+            {
+                tbslot1.Text = "7";
+            }
+            else
+            if (zufallzahl > 1 & zufallzahl <= 3)
+            {
+                tbslot1.Text = "S";
+            }
+            else
+            if (zufallzahl > 3 & zufallzahl <= 5)
+            {
+                tbslot1.Text = "+";
+            }
+            else
+            if (zufallzahl > 5 & zufallzahl <= 7)
+            {
+                tbslot1.Text = "!";
+            }
+            else
+            if (zufallzahl > 7 & zufallzahl <= 10)
+            {
+                tbslot1.Text = "#";
+            }
+
+            zufallzahl = Convert.ToInt32(zufall.Next(1, 11));
+
+            if (zufallzahl == 1)
+            {
+                tbslot2.Text = "7";
+            }
+            else
+            if (zufallzahl > 1 & zufallzahl <= 3)
+            {
+                tbslot2.Text = "S";
+            }
+            else
+            if (zufallzahl > 3 & zufallzahl <= 5)
+            {
+                tbslot2.Text = "+";
+            }
+            else
+            if (zufallzahl > 5 & zufallzahl <= 7)
+            {
+                tbslot2.Text = "!";
+            }
+            else
+            if (zufallzahl > 7 & zufallzahl <= 10)
+            {
+                tbslot2.Text = "#";
+            }
+
+            zufallzahl = Convert.ToInt32(zufall.Next(1, 11));
+
+            if (zufallzahl == 1)
+            {
+                tbslot3.Text = "7";
+            }
+            else
+            if (zufallzahl > 1 & zufallzahl <= 3)
+            {
+                tbslot3.Text = "S";
+            }
+            else
+            if (zufallzahl > 3 & zufallzahl <= 5)
+            {
+                tbslot3.Text = "+";
+            }
+            else
+            if (zufallzahl > 5 & zufallzahl <= 7)
+            {
+                tbslot3.Text = "!";
+            }
+            else
+            if (zufallzahl > 7 & zufallzahl <= 10)
+            {
+                tbslot3.Text = "#";
+            }
+
+            if (tbslot1.Text == "7" & tbslot2.Text == "7" & tbslot3.Text == "7")
+            {
+                punkte = punkte + 60;
+                tbpunkte.Text = Convert.ToString(punkte);
+                MessageBox.Show("7 7 7", "7 7 7", MessageBoxButtons.OK);
+            }
+
+            if (tbslot1.Text == "S" & tbslot2.Text == "S" & tbslot3.Text == "S")
+            {
+                punkte = punkte + 30;
+                tbpunkte.Text = Convert.ToString(punkte);
+            }
+
+            if (tbslot1.Text == "+" & tbslot2.Text == "+" & tbslot3.Text == "+")
+            {
+                punkte = punkte + 20;
+                tbpunkte.Text = Convert.ToString(punkte);
+            }
+
+            if (tbslot1.Text == "!" & tbslot2.Text == "!" & tbslot3.Text == "!")
+            {
+                punkte = punkte + 10;
+                tbpunkte.Text = Convert.ToString(punkte);
+            }
+
+            if (tbslot1.Text == "#" & tbslot2.Text == "#" & tbslot3.Text == "#")
+            {
+                punkte = punkte + 5;
+                tbpunkte.Text = Convert.ToString(punkte);
+            }
+        }
+    }
+}
